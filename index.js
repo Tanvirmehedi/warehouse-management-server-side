80ed8bcff984df6d5690ebd29362e0279349b8f6
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-28 22:08:14 +0600

    Initialijation server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+// middleware intermigration
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WareHouse Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log("Listening on Port", port);
+});

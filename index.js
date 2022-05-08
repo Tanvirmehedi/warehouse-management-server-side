60c35c65eb03f53341b5a4570e6e21c6736af17d
Tanvirmehedi <tjmehedi201@gmail.com>
2022-05-08 14:10:45 +0600

    Database Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 const port = process.env.PORT || 4000;
@@ -9,6 +10,20 @@ const app = express();
 // middleware intermigration
 app.use(cors());
 app.use(express.json());
+
+// DATABASE CONNECTION AND API
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5olip.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("BookHouse Db Connected");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("BookHouse Server Is Running");
